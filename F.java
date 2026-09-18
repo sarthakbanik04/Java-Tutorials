cc91a37db34349532646dc3bb9a1c4282846eb22
Sarthak Banik <sarthakbanik04@gmail.com>
2026-09-18 16:34:32 +0530

    Add files via upload

---
+++ F.java
@@ -0,0 +1,14 @@
+public class F{
+    public static void main(String[] args) {
+        int a=0,b=1;
+        int c;
+        while(a<100){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+            
+        }
+
+    }
+}
