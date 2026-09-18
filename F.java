409d6b7cd727cd9b6fdeaeee46ff459a9a553f47
Sarthak Banik <sarthakbanik04@gmail.com>
2026-09-18 17:00:11 +0530

    Limit Fibonacci sequence output to 10 terms

--- F.java
+++ F.java
@@ -1,12 +1,13 @@
 public class F{
     public static void main(String[] args) {
         int a=0,b=1;
-        int c;
-        while(a<100){
+        int count=0,c;
+        while(count<10){
             System.out.println(a);
             c=a+b;
             a=b;
             b=c;
+            count++;
             
         }
 
