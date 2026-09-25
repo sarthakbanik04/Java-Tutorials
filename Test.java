b6d60cf45871d541670dd2542c2c0b565967e222
Sarthak Banik <sarthakbanik04@gmail.com>
2026-09-25 11:55:16 +0530

    Type Casting Examples

---
+++ Test.java
@@ -0,0 +1,24 @@
+// public class Test{
+//     public static void main(String args[]){
+//         double a=295.04;
+//         byte c=(byte)a;
+//         System.out.println(c);
+//     }
+// }
+// public class Test{
+//     public static void main(String args[]){
+//         char ch='B';
+//         int num=(ch+2);
+//         System.out.println(num);
+//     }
+// }
+public class Test{
+    public static void main(String args[]){
+        short a=10;
+        short b=20;
+        short c=(a+b);
+        System.out.println(c);
+    }
+}
+
+
