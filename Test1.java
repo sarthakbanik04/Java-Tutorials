a793d1c97701dea4a4c5990e7379647e6a519684
Sarthak Banik <sarthakbanik04@gmail.com>
2026-09-25 12:14:00 +0530

    More Type Casting Example.

---
+++ Test1.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String args[]){
+        int[] integers={1,2,3};
+        double[] decimals=(double[])integers;
+        System.out.println(decimals[0]);
+    }
+}
